afe1834b0d86a8fa2e0f96abcd4c9eae3550c0c0
jan <d_jan@ymail.com>
2015-05-22 18:27:16 +0200

    initial

---
+++ svgToSchwarzplan.js
@@ -0,0 +1,99 @@
+var makeRequest = function(){
+	var mapnik_minlon = document.querySelector("#mapnik_minlon").getAttribute("value");
+	var mapnik_minlat = document.querySelector("#mapnik_minlat").getAttribute("value");
+	var mapnik_maxlon = document.querySelector("#mapnik_maxlon").getAttribute("value");
+	var mapnik_maxlat = document.querySelector("#mapnik_maxlat").getAttribute("value");
+	var mapnik_scale  = document.querySelector("#mapnik_scale").value;
+
+	$.ajax({
+		url: "http://render.openstreetmap.org/cgi-bin/export"+"?"+"bbox="+mapnik_minlon+","+mapnik_minlat+","+mapnik_maxlon+","+mapnik_maxlat+"&scale="+mapnik_scale+"&format=svg",
+		method:"GET",
+		dataType:"XML"
+	}).done(processSVG);
+};
+
+
+var processSVG = function(data){
+	deleteUnusedElement(data);
+	setBackgroundColor(data);
+	changePaths(data);
+	download("schwarzplan.svg",new XMLSerializer().serializeToString(data));
+};
+
+var checkIsBuilding = function(element){
+	//checks if the element is a building
+	if(!(element instanceof SVGElement)){return undefined;}
+	var isBuilding = false;
+
+	var buildingcolors= ["fill:rgb(85.098039%,81.568627%,78.823529%)","fill:(81.960784%,77.647059%,74.117647%)"];
+	buildingcolors.forEach(function(color, index, array){
+		if((element.getAttribute("style")).indexOf(color) !== -1){
+			isBuilding = true;
+		}
+	});
+	return isBuilding;
+};
+
+
+var deleteUnusedElement = function(svgdocument){
+//delete defs and other non-path types
+	var defs = svgdocument.querySelector("defs");
+	var use = svgdocument.querySelectorAll("use");
+	if(defs){
+		defs.parentNode.removeChild(defs);
+	}
+
+	for(var i=0; i< use.length; i++){
+		use[i].parentNode.removeChild(use[i]);
+	}
+};
+
+var setBackgroundColor = function(svgdocument){
+	//colors the rect in the background
+	var rects = svgdocument.querySelectorAll("rect");
+	for(var i=0; i< rects.length; i++){
+			rects[i].style.fill="rgb(255,255,255)";
+	}
+};
+
+
+var changePaths = function(svgdocument){
+	var paths= svgdocument.querySelectorAll("path");
+
+	for(var i=0; i<paths.length;i++){
+		var currentElement = paths[i];
+		if(!(currentElement instanceof SVGElement)){
+			continue;
+		}
+		if(checkIsBuilding(currentElement)){
+			currentElement.style.fill="rgb(0%,0%,0%)";
+			currentElement.style.strokeWidth = "0";
+		} else {
+			currentElement.parentNode.removeChild(currentElement);
+		}
+	}
+};
+
+//http://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
+var download = function (filename, text){
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(text));
+		pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+};
+
+if(window.location.href.indexOf("openstreetmap.org") == -1){
+	if(confirm("you seem to be not on openstreetmap.org. The Downloader works only there!\n Shall I open openstreetmap?")){
+		window.location.href="http://www.openstreetmap.org/";
+	}
+} else if(confirm("This will download the current view as a Figure-ground diagram. \n The view can be further cut by choosing setting custom dimensions in the share dialog (do NOT press download there).  \n It only works in Openstreetmap on the Standard Layer (right toolbar, ›Layers‹). \n The maps are under Creativecommons BY-SA license. Please consider contributing or donating to openstreetmap.")){
+	makeRequest();
+}
